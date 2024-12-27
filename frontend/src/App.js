2883908f655e0ab9b9b5c5ac93bfb423c2c69d9c
Iooker123 <Anthony1021al@gmail.com>
2024-12-27 13:16:51 +0800

    Dont Know

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <h1>IELTS Speaking Simulator</h1>
         <p>Answer the following questions as if you are in the IELTS speaking test.</p>
-        <a href="page1.js" class = "button">Start</a>
+        <a href="Page1.js" class = "button">Start</a>
       </header>
     </div>
   );
--- frontend/src/page1.js
+++ frontend/src/page1.js
@@ -4,13 +4,6 @@ function App() {
   return (
     <div className="App">
       <title>IELTS Speaking Test Simulator</title>
-      <header className="App-header">
-      <div class="centered-column">
-        <h1>Welcome to the Centered Page</h1>
-        <p>This is a simple centered column layout.</p>
-        <button class="btn">Click Me</button>
-  </div>
-      </header>
     </div>
   );
 }
