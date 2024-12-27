5e7ab8cf9bfd5c9fec7015ad5a7a999ba6a24d03
Iooker123 <Anthony1021al@gmail.com>
2024-12-27 20:47:35 +0800

    Page 1 ver 1

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,6 +9,21 @@ function App() {
         <p>Answer the following questions as if you are in the IELTS speaking test.</p>
         <a href="Page1.js" class = "button">Start</a>
       </header>
+
+      <header className="App-header">
+        <div className="centered-column">
+          <title>IELTS Speaking Test Simulator</title>
+          <h4>Below are the questions for this oral test.</h4>
+          <p class = "detail"> Please click on the button below to start the test when you are ready.</p>
+
+          <div id="backend-content" class="content-placeholder">
+            <p>Title</p>
+          </div>
+          <br></br>
+          <button class="button">Record (Photo replace)</button>
+        </div>
+      </header>
+
     </div>
   );
 }
